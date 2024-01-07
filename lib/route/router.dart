755fd513fcc5a7aae2719374ca62b5377cb23ddf
Cecep Rokani <cecepfahriazal1997@gmail.com>
2024-01-07 16:39:23 +0700

    feat: add route

---
+++ lib/route/router.dart
@@ -0,0 +1,11 @@
+import 'package:go_router/go_router.dart';
+import 'package:hostpitalmarketing/pages/signin.dart';
+import 'package:hostpitalmarketing/pages/main.dart';
+
+final GoRouter router = GoRouter(routes: [
+  GoRoute(path: '/home', builder: (context, state) => const MyApp()),
+  GoRoute(
+    path: '/',
+    builder: (context, state) => Signin(),
+  )
+]);
